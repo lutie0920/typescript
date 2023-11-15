9508cd2199a055e9734e2b6c0a87f0ea59ecf84f
lutie0920 <lucianatie09@gmail.com>
2023-11-15 13:00:44 -0300

    fazer o exercicio

---
+++ src/exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+// uma função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles
+const multiplicacao = (num1, num2) => num1 * num2;
+console.log(multiplicacao(15, 30));
+// e uma função de saudação que deverá receber um nome e retornar a concatenação “Olá “ + nome;
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(saudacao("Luciana"));
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,13 @@
+// uma função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles
+
+const multiplicacao = (num1: number, num2: number): number => num1 * num2;
+
+console.log(multiplicacao(15, 30));
+
+// e uma função de saudação que deverá receber um nome e retornar a concatenação “Olá “ + nome;
+
+function saudacao(nome: string){
+    return "Olá " + nome;
+}
+
+console.log(saudacao("Luciana"));
